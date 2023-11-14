537785def16a25d1fe8690fb7139a42baa071fe2
mrKazzila <mrkazzila@gmail.com>
2023-11-14 15:24:39 +0300

    fear: add mongo settings

---
+++ backend/config/database.py
@@ -0,0 +1,41 @@
+import json
+import logging
+from pathlib import Path
+from sys import exit
+
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCursor,
+    AsyncIOMotorDatabase,
+)
+
+from config.settings import settings
+
+logger = logging.getLogger(__name__)
+
+client: AsyncIOMotorClient = AsyncIOMotorClient(settings().mongo_db.dsn)
+db: AsyncIOMotorDatabase = client.get_database(settings().mongo_db.DB_NAME)
+
+collection_name: AsyncIOMotorCursor = db.get_collection(
+    settings().mongo_db.COLLECTION_NAME,
+)
+
+
+def add_test_templates(
+        json_file: Path,
+        collection_name_: AsyncIOMotorCursor,
+) -> None:
+    try:
+        logger.info(
+            'Start add test data from file %(file)s',
+            {'file': json_file},
+        )
+
+        with open(json_file, 'r') as json_file:
+            test_templates = json.load(json_file)
+        [collection_name_.insert_one(template) for template in test_templates]
+
+        logger.info('Test data successfully added')
+    except Exception as err:
+        logger.error(err)
+        exit(err)
