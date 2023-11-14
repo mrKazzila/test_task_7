c1d1602f8f7be678d9747d04cc4e68e810c071f4
mrKazzila <mrkazzila@gmail.com>
2023-11-14 15:25:04 +0300

    fear: add entrypoint

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+import logging
+from contextlib import asynccontextmanager
+from pathlib import Path
+
+from fastapi import FastAPI
+
+from api.routers_setup import routers_setup
+from config.database import collection_name, add_test_templates
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    logger.info('Service started')
+
+    add_test_templates(
+        json_file=Path(Path(__file__).parent / 'test_forms.json').resolve(),
+        collection_name_=collection_name,
+    )
+
+    yield
+
+    logger.info('Service exited')
+
+
+app = FastAPI(
+    title='Test task',
+    description=(
+        'Create by '
+        '<a href="mailto:kazzilacareer@gmail.com?'
+        'subject=[TestTask]%20🔥%20Feedback&'
+        'body=Hi%20Ilya%2C%0A%0AI%27m%20coming%20to%20you%20today%20'
+        'after%20seeing%20your%20solution%20'
+        'of%20the%20test%20task%20for%20...">'
+        'kazzilacareer@gmail.com'
+        '</a>'
+    ),
+    lifespan=lifespan,
+)
+
+routers_setup(app=app)
