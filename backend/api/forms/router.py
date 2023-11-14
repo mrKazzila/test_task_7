ad5eb8d942dcd7765c89ebde1c8639a4cf5f44e5
mrKazzila <mrkazzila@gmail.com>
2023-11-15 01:20:50 +0300

    feat: add status code, logs and rename some func from services

--- backend/api/forms/router.py
+++ backend/api/forms/router.py
@@ -1,9 +1,9 @@
 import logging
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from api.forms.services import get_field_type_priority, find_matching_template
+from api.forms.services import set_types_for_field, find_matching_template
 
 logger = logging.getLogger(__name__)
 
@@ -17,17 +17,23 @@ router = APIRouter(
     name='Find form',
     description='Find form by data',
 )
-async def find_form_by_data(data: dict) -> JSONResponse:
+async def find_form_by_data(data: dict[str, str]) -> JSONResponse:
+    sub_data = data
+
     try:
-        if template_name := await find_matching_template(data=data):
+
+        if template_name := await find_matching_template(data=sub_data):
             return JSONResponse(
-                content={
-                    'template_name': template_name,
-                },
+                content={'template_name': template_name},
+                status_code=status.HTTP_200_OK,
             )
 
+        logger.debug('Form not found, add types for fields')
+        field_types = set_types_for_field(data=sub_data)
         return JSONResponse(
-            content=get_field_type_priority(keys=data.keys()),
+            content=field_types,
+            status_code=status.HTTP_201_CREATED,
         )
+
     except HTTPException as err:
         logger.error(err)
