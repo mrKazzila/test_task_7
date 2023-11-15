134aa5524260147dd41f8241d503e239ee85575d
mrKazzila <mrkazzila@gmail.com>
2023-11-15 14:02:41 +0300

    refactor: format str

--- backend/api/forms/validators.py
+++ backend/api/forms/validators.py
@@ -2,7 +2,9 @@ import re
 
 __DATA_REGEX_PATTERN = re.compile(r'^(\d{2}\.\d{2}.\d{4}|\d{4}-\d{2}-\d{2})$')
 __PHONE_REGEX_PATTERN = re.compile(r'^\+\d \d{3} \d{3} \d{2} \d{2}$')
-__EMAIL_REGEX_PATTERN = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+__EMAIL_REGEX_PATTERN = re.compile(
+    r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+)
 
 
 def validate_date(date: str) -> bool:
